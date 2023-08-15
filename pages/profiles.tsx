31b35df32b96d13721f89e73ffa1c7ff28f93da0
AdeboyeJuliet <julietadeboye01@gmail.com>
2023-08-15 12:56:29 +0100

    feat: navigation menu

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -6,20 +6,20 @@ import { useRouter } from "next/router";
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
-
+  
     if (!session) {
-        return {
-            redirect: {
-                destination: '/auth',
-                permanent: false,
-            }
+      return {
+        redirect: {
+          destination: '/auth',
+          permanent: false,
         }
+      }
     }
-
+  
     return {
-        props: {}
+      props: {}
     }
-}
+  }
 
 const Profiles = () => {
     const router = useRouter();
