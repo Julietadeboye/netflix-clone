ccd20f6b6057abcad084e8ec2715e5830c0d6269
Juliet <julietadeboye01@gmail.com>
2023-08-24 15:42:02 +0100

    feat: created random api for billboard.tsx

---
+++ components/Billboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import useBillboard from '@/hooks/useBillboard';
+
+const Billboard = () => {
+  const { data } = useBillboard();
+  
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Billboard;
